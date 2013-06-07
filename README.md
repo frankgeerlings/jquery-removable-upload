9d64d05601e292b0aeecfbb2011a818781701ebc
Frank Geerlings <frank@frankgeerlings.com>
2013-06-07 20:29:48 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+jQuery removable upload
+-
+This is a trivial jQuery plugin that will add a button next to a file input field, allowing the file input field to be reset, so as to add an *undo* function.
+
+Without this, not all browsers allow you to unselect a file once you have chosen one.
---
+++ jquery.removable.js
@@ -0,0 +1,23 @@
+﻿(function ($) {
+    /**
+    * Removable attachments — allows file upload fields to be cleared.
+    */
+    $.fn.removable = function () {
+        var removeAndRecreate = function ($element) {
+            $element.replaceWith($element.val('').clone(true));
+        };
+
+        var clicked = function () {
+            var self = this;
+            var $self = $(self);
+
+            var $a = $('<a class="removeAttachment" href="#" title="Cancel, do not upload."><span></span></a>').insertBefore($self);
+            $a.click(function () {
+                $a.remove();
+                removeAndRecreate($self);
+            });
+        };
+
+        return this.on("change", "input[type='file']", clicked);
+    };
+})(jQuery);
